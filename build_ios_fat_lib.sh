bc0a56933a9791579dad4760fe61d058cd03a00b
cfogelklou <chris.fogelklou@gmail.com>
2015-06-17 13:26:55 +0200

    Move the executable file around to a more logical place.

--- build_ios_fat_lib.sh
+++ build_ios_fat_lib.sh
@@ -1,10 +1,10 @@
-rm libremote_io.a
+rm _ios/libremote_io.a
 make -f Makefile_ios.mak clean
 make -f Makefile_ios.mak ARCH=armv6
 make -f Makefile_ios.mak ARCH=x86_64 SIM=1
 make -f Makefile_ios.mak ARCH=armv7
 make -f Makefile_ios.mak ARCH=armv7s
 make -f Makefile_ios.mak ARCH=arm64
-lipo -create _ios/armv7/libremote_io.a _ios/armv7s/libremote_io.a _ios/arm64/libremote_io.a _ios/x86_64/libremote_io.a _ios/armv6/libremote_io.a -output libremote_io.a
-cp libremote_io.a remoteio_test/
+lipo -create _ios/armv7/libremote_io.a _ios/armv7s/libremote_io.a _ios/arm64/libremote_io.a _ios/x86_64/libremote_io.a _ios/armv6/libremote_io.a -output _ios/libremote_io.a
+cp _ios/libremote_io.a remoteio_test/
 
