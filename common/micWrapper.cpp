421c6ccc4bea189d65d313aaaa7d98e765fd3741
cfogelklou <chris.fogelklou@gmail.com>
2017-07-24 15:30:19 +0200

    Refactor project.

--- common/micWrapper.cpp
+++ common/micWrapper.cpp
@@ -4,7 +4,8 @@
 //  Created by Chris Fogelklou
 //
 //
-#if defined (__ANDROID__) || defined(ROBOVM)
+
+#if defined(__ANDROID__) //|| defined(ROBOVM)
 #include "../common/micwrapper_MicW.h"
 #include "PcmQ.h"
 #include "remoteio_mic_c.h"
